6c4d1efd903147915c9b7da8a8f09927d40b3e59
Manavitaliya <manp6053@gmail.com>
2026-03-27 14:24:45 +0530

    ini

--- HostelApp/views.py
+++ HostelApp/views.py
@@ -14,7 +14,7 @@ from django.shortcuts import get_object_or_404
 # --------------------TRY VIEWS-------------------
 
 def check(request):
-    return render(request, 'student/apply_hostel.html')
+    return render(request, 'student/dashboard.html')
 
 # -----------------HOME --------------------
 
