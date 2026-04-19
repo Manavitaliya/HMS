cdaf90bd9170e26bd391a85e306905093ba6ac30
Manavitaliya <manp6053@gmail.com>
2026-04-19 17:47:17 +0530

    change

--- HostelApp/models.py
+++ HostelApp/models.py
@@ -21,10 +21,12 @@ class User(AbstractUser):
 # ---------------- HOSTEL MODEL ----------------
 
 class Hostel(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True)
+    location = models.CharField(max_length=200, null=True)
     address = models.TextField(null=True, blank=True)
-    total_rooms = models.IntegerField()
+    total_rooms = models.IntegerField(null=True)
     monthly_fee = models.DecimalField(max_digits=10, decimal_places=2)
+    available_rooms = models.IntegerField(null=True) 
 
     def __str__(self):
         return self.name
